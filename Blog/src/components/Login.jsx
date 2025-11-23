43d4bc371298f2f81feaf86a2b39f1000cdf0e80
Anishneupane <neupaneanish79@gmail.com>
2025-11-23 20:29:44 +0545

    code  added in login file

--- Blog/src/components/Login.jsx
+++ Blog/src/components/Login.jsx
@@ -27,9 +27,40 @@ function Login() {
       setError(error.message);
     }
   };
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div className="mb-2 flex justify-center">
+        <span className="inline-block w-full max-w-[100px]">
+          <Logo width="100%" />
+        </span>
+      </div>
+      <h2 className="text-center text-2xl font-bold leading-tight">
+        Sign in to your account
+      </h2>
+      <p className="mt-2 text-center text-base text-black/60">
+        Don&apos;t have any account?&nbsp;
+        <Link
+          to="/signup"
+          className="font-medium text-primary transition-all duration-200 hover:underline"
+        >
+          Sign Up
+        </Link>
+      </p>
+      {error && <p className="text-red-500 text-center mt-8">{error}</p>}
+      <form onSubmit={handleSubmit(login)} className="mt-8">
+        <div className="space-y-5">
+          <Input
+            label="Email: "
+            placeholder="Enter your email"
+            type="email"
+            {...register("email")}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
 
-// Start Time of video  = 24:00 ;
+// Video start time : 30:10 .
